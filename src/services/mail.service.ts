fb62652f885293b39c00d7df7a61e6641ad7561c
Đỗ Công Sơn <sonanhson99@gmail.com>
2024-06-22 17:22:19 +0700

    forgot password

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,8 +1,14 @@
 import nodemailer from 'nodemailer'
-import logger from '../config/logger.config.js'
 import dotenv from 'dotenv'
+import ejs from 'ejs'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
 import * as process from 'node:process'
+import logger from '../config/logger.config.js'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 dotenv.config()
 
 const transport = nodemailer.createTransport({
@@ -24,35 +30,37 @@ if (process.env.NODE_ENV !== 'test') {
     })
 }
 
-const sendEmail = async (to: string, subject: string, text: string): Promise<void> => {
-  const msg = { from: process.env.SMTP_FROM, to, subject, text }
+const sendEmail = async (to: string, subject: string, html: string): Promise<void> => {
+  const msg = { from: process.env.SMTP_FROM, to, subject, html }
   await transport.sendMail(msg)
 }
 
 const sendResetPasswordEmail = async (to: string, token: string): Promise<void> => {
   const subject = 'Reset password'
   const resetPasswordUrl = `${process.env.API_URL}/auth/reset-password?email=${to}&token=${token}`
-  const text = `Dear user,
-To reset your password, click on this link: ${resetPasswordUrl}
-If you did not request any password resets, then ignore this email.`
-  await sendEmail(to, subject, text)
+
+  const templatePath = path.join(__dirname, '../views/email', 'resetPassword.ejs')
+  const template = fs.readFileSync(templatePath, 'utf-8')
+  const html = ejs.render(template, { resetPasswordUrl })
+
+  await sendEmail(to, subject, html)
 }
 
 const sendVerificationEmail = async (to: string, token: string): Promise<void> => {
   const subject = 'Email Verification'
   const verificationEmailUrl = `${process.env.API_URL}/auth/verify-email?token=${token}`
-  const text = `Dear user,
+  const html = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`
-  await sendEmail(to, subject, text)
+  await sendEmail(to, subject, html)
 }
 
 const sendNewPasswordEmail = async (to: string, password: string): Promise<void> => {
   const subject = 'New Password'
-  const text = `Dear user,
+  const html = `Dear user,
 Your new password is: ${password}
 Please change it after you login.`
-  await sendEmail(to, subject, text)
+  await sendEmail(to, subject, html)
 }
 
 export { transport, sendEmail, sendResetPasswordEmail, sendVerificationEmail, sendNewPasswordEmail }
